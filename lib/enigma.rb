67cb05ff632646ab392e6b7e739b176efe605c86
Harrison Levin <harrison483@gmail.com>
2019-11-04 12:39:45 -0700

    working through possible combinations (256)

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -47,8 +47,55 @@ class Enigma
     index_to_character(new_index_values).join
   end
 
-  def crack
+  def crack(encrypted_text, date=today_date)
+    @date = date.to_s
+    @keys = keys.to_s
+    @encrypted_text = encrypted_text
+    @message = cracker(encrypted_text, date)
+    {decryption: message, key: keys, date: date}
+  end
 
+  def cracker(encrypted_text, date)
+    indexed_chars = index_text(encrypted_text)
+    last_four_index = index_text(" end")
+    last_four_chars = indexed_chars.last(4)
+    total_offset = last_four_chars.reduce([]) do |acc, letter_index|
+      acc << ((letter_index[1] - last_four_index[0][1]) < 1 ? ((letter_index[1] + 27) - last_four_index[0][1] ) : (letter_index[1] - last_four_index[0][1]))
+      last_four_index.rotate!
+      acc.rotate(2)
+    end
+    offset_digits = create_offset_digits
+    numbers = (00..99).to_a
+    a = []
+    b = []
+    c = []
+    d = []
+    numbers.map do |num|
+      a << num if (((num + offset_digits[0].to_i) % 27) == total_offset[0])
+      b << num if (((num + offset_digits[1].to_i) % 27) == total_offset[1])
+      c << num if (((num + offset_digits[2].to_i) % 27) == total_offset[2])
+      d << num if (((num + offset_digits[3].to_i) % 27) == total_offset[3])
+    end
+
+
+    possible_outcomes = []
+      a.each do |n1|
+        b.each do |n2|
+          c.each do |n3|
+            d.each do |n4|
+              possible_outcomes.push([n1, n2, n3, n4])
+            end
+          end
+        end
+      end
+    # first_fig = a.select do |first|
+    #   first if b.select do |second|
+    #     second.to_s.chars.first.to_i == first.to_s.chars.first.to_i
+    #     end
+    # end
+    require "pry"; binding.pry
+
+    #decrypt(encrypted_text, keys, date)
   end
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -7,23 +7,23 @@ class TestEnigma < Minitest::Test
     @enigma = Enigma.new
   end
 
-  def test_it_exists
-    assert_instance_of Enigma, @enigma
-  end
-
-  def test_encrypt
-    assert_equal ({ encryption: "keder ohulw",
-      key: "02715",
-      date: "040895" }), @enigma.encrypt('hello world', "02715", "040895")
-    assert_equal 'keder ohulw', @enigma.encrypted_text
-  end
-
-  def test_decrypt
-    assert_equal ({ decryption: "hello world",
-      key: "02715",
-      date: "040895" }), @enigma.decrypt("keder ohulw", "02715", "040895")
-    assert_equal 'hello world', @enigma.message
-  end
+  # def test_it_exists
+  #   assert_instance_of Enigma, @enigma
+  # end
+  #
+  # def test_encrypt
+  #   assert_equal ({ encryption: "keder ohulw",
+  #     key: "02715",
+  #     date: "040895" }), @enigma.encrypt('hello world', "02715", "040895")
+  #   assert_equal 'keder ohulw', @enigma.encrypted_text
+  # end
+  #
+  # def test_decrypt
+  #   assert_equal ({ decryption: "hello world",
+  #     key: "02715",
+  #     date: "040895" }), @enigma.decrypt("keder ohulw", "02715", "040895")
+  #   assert_equal 'hello world', @enigma.message
+  # end
 
   def test_crack
     assert_equal ({:encryption=>"vjqtbeaweqihssi",
@@ -32,7 +32,7 @@ class TestEnigma < Minitest::Test
     assert_equal 'hello world end', @enigma.message
     assert_equal ({:decryption=>"hello world end",
        :key=>"08304",
-       :date=>"291018"}), @enigma.crack("hello world end", "291018")
+       :date=>"291018"}), @enigma.crack("vjqtbeaweqihssi", "291018")
   end
 
 end
