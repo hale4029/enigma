c95edb593cc90befe31b15ed6f73d35b1e564064
Harrison Levin <harrison483@gmail.com>
2019-11-04 16:49:25 -0700

    moved methods over to crack module -- hashed uit methods in engima

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,6 @@
 require './lib/helper_module'
 require './lib/crack_module'
-  
+
 class Enigma
   include HelperMethods
   include CrackMethods
@@ -60,51 +60,51 @@ class Enigma
     indexed_chars = index_text(encrypted_text)
     last_four_index = index_text(" end")
     last_four_chars = indexed_chars.last(4)
-    total_offset = last_four_chars.reduce([]) do |acc, letter_index|
+    @total_offset = last_four_chars.reduce([]) do |acc, letter_index|
       acc << ((letter_index[1] - last_four_index[0][1]) < 1 ? ((letter_index[1] + 27) - last_four_index[0][1] ) : (letter_index[1] - last_four_index[0][1]))
       last_four_index.rotate!
       acc.rotate(2)
     end
+    @a = []; @b = [], @c = [], @d = []
+    solve_for_key_offset
+    # possible_combinations
+    # key_iterations
+    find_key
+    # offset_digits = create_offset_digits
+    # number_set = (00..99).to_a
+    # number_set.map do |num|
+    #   a << num.to_s.rjust(2,'0') if (((num + offset_digits[0].to_i) % 27) == total_offset[0])
+    #   b << num.to_s.rjust(2,'0') if (((num + offset_digits[1].to_i) % 27) == total_offset[1])
+    #   c << num.to_s.rjust(2,'0') if (((num + offset_digits[2].to_i) % 27) == total_offset[2])
+    #   d << num.to_s.rjust(2,'0') if (((num + offset_digits[3].to_i) % 27) == total_offset[3])
+    # end
 
-    a = []
-    b = []
-    c = []
-    d = []
-    offset_digits = create_offset_digits
-    number_set = (00..99).to_a
-    number_set.map do |num|
-      a << num.to_s.rjust(2,'0') if (((num + offset_digits[0].to_i) % 27) == total_offset[0])
-      b << num.to_s.rjust(2,'0') if (((num + offset_digits[1].to_i) % 27) == total_offset[1])
-      c << num.to_s.rjust(2,'0') if (((num + offset_digits[2].to_i) % 27) == total_offset[2])
-      d << num.to_s.rjust(2,'0') if (((num + offset_digits[3].to_i) % 27) == total_offset[3])
-    end
+    # possible_combinations = []
+    #   a.each do |n1|
+    #     b.each do |n2|
+    #       c.each do |n3|
+    #         d.each do |n4|
+    #           possible_combinations.push([n1, n2, n3, n4])
+    #         end
+    #       end
+    #     end
+    #   end
 
-    possible_combinations = []
-      a.each do |n1|
-        b.each do |n2|
-          c.each do |n3|
-            d.each do |n4|
-              possible_combinations.push([n1, n2, n3, n4])
-            end
-          end
-        end
-      end
+    # key_possibilities = possible_combinations.reduce([]) do |acc, key_iteration|
+    #   acc << [key_iteration.join.rjust(8,"0").chars[0],
+    #   key_iteration.join.rjust(8,"0").chars[1],
+    #   key_iteration.join.rjust(8,"0").chars[3],
+    #   key_iteration.join.rjust(8,"0").chars[5],
+    #   key_iteration.join.rjust(8,"0").chars[7]].join
+    #   acc
+    # end
 
-    key_possibilities = possible_combinations.reduce([]) do |acc, key_iteration|
-      acc << [key_iteration.join.rjust(8,"0").chars[0],
-      key_iteration.join.rjust(8,"0").chars[1],
-      key_iteration.join.rjust(8,"0").chars[3],
-      key_iteration.join.rjust(8,"0").chars[5],
-      key_iteration.join.rjust(8,"0").chars[7]].join
-      acc
-    end
-
-    hash_output = key_possibilities.map do |key_poss|
-      @keys = key_poss
-      key_poss if decode("ssih") == "end "
-    end.compact
-
-    @keys = hash_output.first
+    # hash_output = key_iterations.map do |key_poss|
+    #   @keys = key_poss
+    #   key_poss if decode("ssih") == "end "
+    # end.compact
+    #
+    # @keys = hash_output.first
 
     decode(encrypted_text)
   end
